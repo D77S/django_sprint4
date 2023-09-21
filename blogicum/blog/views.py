c0a35b2363382df4653658a33b060b6f376034dc
Дмитрий <d77s@inbox.ru>
2023-09-21 20:04:40 +0300

    Next

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Count, QuerySet
 from django.http import Http404, HttpResponse
@@ -6,8 +8,6 @@ from django.urls import reverse, reverse_lazy
 from django.utils import timezone
 from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
                                   UpdateView)
-
-from typing import Any
 
 from .forms import CommentForm, PostForm, UserUpdateForm
 from .models import Category, Comment, Post, User
@@ -23,10 +23,8 @@ def author_selected(self):
 
 def posts_just_selected() -> QuerySet:
     """
-    Возвращает queryset модели Post
-    с заджойненными к ней моделями
-    Category, Location, User,
-    без фильтрации,
+    Возвращает queryset модели Post с заджойненными к ней моделями
+    Category, Location, User, без фильтрации,
     только сортировка по дате публикации."""
     return Post.objects.select_related(
         'category',
@@ -37,10 +35,8 @@ def posts_just_selected() -> QuerySet:
 
 def posts_selected() -> QuerySet:
     """
-    Возвращает queryset модели Post
-    с заджойненными к ней моделями
-    Category, Location, User,
-    с фильтрацией,
+    Возвращает queryset модели Post с заджойненными к ней моделями
+    Category, Location, User, с фильтрацией,
     сортировка по дате публикации,
     с присоединенным полем счетчика каментов."""
     return posts_just_selected().filter(
@@ -52,10 +48,8 @@ def posts_selected() -> QuerySet:
 
 def posts_selected_with_unpublished_and_future() -> QuerySet:
     """
-    Возвращает queryset модели Post
-    с заджойненными к ней моделями
-    Category, Location, User,
-    без фильтрации,
+    Возвращает queryset модели Post с заджойненными к ней моделями
+    Category, Location, User, без фильтрации,
     сортировка по дате публикации,
     с присоединенным полем счетчика каментов."""
     return posts_just_selected().annotate(
@@ -76,27 +70,30 @@ class DispatchPostMixin:
     надо убедиться, что на действие претендует автор.
     """
     def dispatch(self, request, *args, **kwargs):
-        instance = get_object_or_404(Post, pk=kwargs['pk'])
-        if instance.author != request.user:
+        if self.get_object().author != request.user:  # type: ignore
             return redirect(
                 reverse('blog:post_detail',
                         kwargs={'pk': self.kwargs['pk']}))  # type: ignore
         return super().dispatch(request, *args, **kwargs)  # type: ignore
 
 
+class DispatchCommentMixin:
+    """
+    Миксин переопределения диспетчера
+    по проверке на авторство каментов там, где
+    надо убедиться, что на действие претендует автор.
+    """
+    def dispatch(self, request, *args, **kwargs):
+        if self.get_object().author != request.user:  # type: ignore
+            return redirect(
+                reverse('blog:post_detail',
+                        kwargs={'pk': self.kwargs['post_pk']}))  # type: ignore
+        return super().dispatch(request, *args, **kwargs)  # type: ignore
+
+
 class IndexView(PaginateMixin, ListView):
     """Класс для CBV, которая
-    отображает главную страницу.
-
-    Наследован от стандартного ListView,
-    но переопределяем:
-    - имя шаблона показывания,
-    - величину пагинирования,
-    - queryset показываемый, в который мы кладем
-    queryset модели Post с заджойненными к ней моделями
-    Category, Location, User, с фильтрацией,
-    сортировка по дате публикации."""
-
+    отображает главную страницу."""
     template_name = 'blog/index.html'
 
     def get_queryset(self) -> QuerySet:
@@ -105,19 +102,7 @@ class IndexView(PaginateMixin, ListView):
 
 class CategoryView(PaginateMixin, ListView):
     """Класс для CBV, которая
-    отображает все (почти) посты заданной категории.
-
-    Наследован от стандартного ListView,
-    но переопределяем:
-    - имя шаблона показывания,
-    - величину пагинирования,
-    - queryset показываемый, в который мы кладем
-    queryset модели Post с заджойненными к ней моделями
-    Category, Location, User, с фильтрацией,
-    сортировка по дате публикации.
-    А перед его вычислением - получаем объект модели категорий,
-    используя имя категории, переданное сюда в упаковке kwargs."""
-
+    отображает все (почти) посты заданной категории."""
     template_name = 'blog/category.html'
 
     def get_queryset(self) -> QuerySet:
@@ -141,24 +126,7 @@ class CategoryView(PaginateMixin, ListView):
 class UserDetailView(PaginateMixin, ListView):
     """Класс для CBV, которая
     отображает детализированную информацию
-    об одном конкретном пользователе.
-
-    Наследован от стандартного DetailView,
-    но переопределяем:
-    - имя шаблона показывания,
-    - имя переменной в urls, в которую принят идентификатор объекта модели,
-    - имя поля модели, с которым надо сопоставлять предыдущий, когда он slug,
-    - имя переменной контекста, через которую транспортируем всё в шаблон,
-    - величину пагинирования,
-    - queryset показываемый, в который мы кладем
-    queryset модели Post с заджойненными к ней моделями
-    Category, Location, User, без фильтрации,
-    сортировка по дате публикации, отфильтровав одного нужного автора.
-    А перед его вычислением - получаем объект модели юзеров,
-    используя имя автора, переданное сюда в упаковке kwargs.
-
-    И еще увеличиваем контекст,
-    добавив к родительскому объект нужного автора."""
+    об одном конкретном пользователе."""
 
     template_name = 'blog/profile.html'
     slug_url_kwarg = 'username'
@@ -178,18 +146,7 @@ class UserDetailView(PaginateMixin, ListView):
 
 class UserUpdateView(LoginRequiredMixin, UpdateView):
     """Класс для CBV, которая
-    апдейтит профиль залогиненного юзера.
-
-    Наследован от стандартного UpdateView,
-    и еще от LoginRequiredMixin, так как апдейтить профиль (свой!)
-    разрешено только залогиненному юзеру.
-
-    Переопределяем:
-    - форму показываемую,
-    - имя шаблона показывания,
-    - локатор при успешном завершении обратно из шаблона,
-    - объект показываемый, в который мы кладем объект юзера из запроса.
-    """
+    апдейтит профиль залогиненного юзера."""
     form_class = UserUpdateForm
     template_name = 'blog/user.html'
     success_url = reverse_lazy('blog:index')
@@ -200,21 +157,7 @@ class UserUpdateView(LoginRequiredMixin, UpdateView):
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     """Класс для CBV, которая
-    создает новый пост залогиненного юзера.
-
-    Наследован от стандартного DetailView,
-    и еще от LoginRequiredMixin, так как создавать новый пост
-    разрешено только залогиненному юзеру.
-
-     Переопределяем:
-    - модель, куда потом сунем созданный объект,
-    - форму показываемую,
-    - имя шаблона показывания,
-    - метод валидации формы, чтобы он дополнительно к родительскому еще делал:
-        - юзера в нее помещаем из залогиненного,
-        - и еще проверяем, что юзер не установил pub_date в прошлом,
-    - локатор при успешном завершении обратно из шаблона.
-    """
+    создает новый пост залогиненного юзера."""
     model = Post
     form_class = PostForm
     template_name = 'blog/create.html'
@@ -235,18 +178,7 @@ class PostDetailView(DetailView):
     """Класс для CBV, которая
     отображает все данные
     по одному конкретному посту,
-    включая каменты к нему.
-
-    Переопределяем:
-    - объект показываемый, в который мы кладем объект модели Post,
-    по ключу pk из пути, заджойнив к нему категорию, автора, локацию.
-    Если только залогиненный юзер - автор поста,
-    время опубликации уже наступило, и категория не снята.
-
-    И еще увеличиваем контекст,
-    добавив к родительскому:
-    - объект формы камента;
-    - все уже имеющиеся каменты данного поста."""
+    включая каменты к нему."""
 
     template_name = 'blog/detail.html'
 
@@ -269,16 +201,7 @@ class PostDetailView(DetailView):
 
 class PostUpdateView(DispatchPostMixin, LoginRequiredMixin, UpdateView):
     """Класс для CBV, которая
-    редактирует пост, если залогинен его автор.
-
-    Наследован от стандартного UpdateView.
-
-    Переопределяем:
-    - локатор при успешном завершении обратно из шаблона -
-    на только что проапдейтенный пост,
-    - дистпетчера чтоб не прошел неавтор,
-    - валидатор формы для проверки, что не вписали прошедшее время публикации.
-    """
+    редактирует пост, если залогинен его автор."""
 
     template_name = 'blog/create.html'
     model = Post
@@ -296,20 +219,7 @@ class PostUpdateView(DispatchPostMixin, LoginRequiredMixin, UpdateView):
 
 class PostDeleteView(DispatchPostMixin, LoginRequiredMixin, DeleteView):
     """Класс для CBV, которая
-    удаляет пост залогиненного юзера.
-
-    Наследован от стандартного DeleteView,
-    и еще от LoginRequiredMixin, так как удалить пост
-    разрешено только залогиненному юзеру.
-    Перед удалением поста должна открываться
-    подтверждающая страница, содержащая удаляемый пост.
-
-    Переопределяем:
-    - модель используемую,
-    - шаблон показываемый,
-    - локатор успешного завершения шаблона,
-    - дистпетчера чтоб не прошел неавтор.
-    """
+    удаляет пост залогиненного юзера."""
     model = Post
     template_name = 'blog/create.html'
     success_url = reverse_lazy('blog:profile')
@@ -322,17 +232,7 @@ class PostDeleteView(DispatchPostMixin, LoginRequiredMixin, DeleteView):
 
 class CommentCreateView(LoginRequiredMixin, CreateView):
     """Класс для CBV, которая
-    создает комментарий залогиненного юзера.
-
-    Наследован от стандартного CreateView,
-    и еще от LoginRequiredMixin, так как создать камент
-    разрешено только залогиненному юзеру.
-
-    Переопределяем:
-    - локатор успешного завершения шаблона,
-    - валидатор формы.
-
-    """
+    создает комментарий залогиненного юзера."""
     model = Comment
     form_class = CommentForm
     template_name = 'includes/comments.html'
@@ -347,33 +247,9 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class DispatchCommentMixin:
-    """
-    Миксин переопределения диспетчера
-    по проверке на авторство каментов там, где
-    надо убедиться, что на действие претендует автор.
-    """
-    def dispatch(self, request, *args, **kwargs):
-        if self.get_object().author != request.user:
-            return redirect(
-                reverse('blog:post_detail',
-                        kwargs={'pk': self.kwargs['post_pk']}))  # type: ignore
-        return super().dispatch(request, *args, **kwargs)  # type: ignore
-
-
 class CommentUpdateView(DispatchCommentMixin, LoginRequiredMixin, UpdateView):
     """Класс для CBV, которая
-    редактирует комментарий залогиненного юзера.
-
-    Наследован от стандартного UpdateView,
-    и еще от LoginRequiredMixin, так как редактировать камент
-    разрешено только залогиненному юзеру.
-
-    Переопределяем:
-    - диспетчера, что не прошел неавтор,
-    - объект в шаблон, один нужный камент,
-    - локатор успешного завершения шаблона.
-    """
+    редактирует комментарий залогиненного юзера."""
     form_class = CommentForm
     template_name = 'blog/comment.html'
 
@@ -387,16 +263,7 @@ class CommentUpdateView(DispatchCommentMixin, LoginRequiredMixin, UpdateView):
 
 class CommentDeleteView(DispatchCommentMixin, LoginRequiredMixin, DeleteView):
     """Класс для CBV, которая
-    удаляет комментарий залогиненного юзера.
-
-    Наследован от стандартного DeleteView,
-    и еще от LoginRequiredMixin, так как удалять камент
-    разрешено только залогиненному юзеру.
-
-    Переопределяем:
-    - диспетчера, что не прошел неавтор,
-    - локатор успешного завершения шаблона.
-    """
+    удаляет комментарий залогиненного юзера."""
     template_name = 'blog/comment.html'
 
     def get_object(self, queryset=None):
